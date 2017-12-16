07a612bc860260c5d833380655a2599d8bc4faeb
“Pianist038801” <maximivanov818@gmail.com>
2017-12-16 05:34:26 +0400

    OnFixing Phase2

--- src/App_main.js
+++ src/App_main.js
@@ -3,7 +3,7 @@ import FontAwesome from 'react-fontawesome';
 import getData from './api';
 import { Card, Icon, Image } from 'semantic-ui-react';
 import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+
 import ToolTip from './components/ToolTip.js';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
@@ -23,7 +23,7 @@ class App extends Component {
 			data.push(temp);
 		}
 		this.getDataFromServer(data);
-		this.state = { open: false, data: [], id: 1, propertyName: 'part no' };
+		this.state = { field: props.location.state.data[0], open: false, data: [], id: 1, propertyName: 'part no' };
 	}
 	componentDidMount() {}
 	async getDataFromServer(data) {
@@ -70,81 +70,78 @@ class App extends Component {
 		return 'grey';
 	};
 	render() {
-		let children = [];
-		let second = [ 'ABC   10 items   in 5 days', 'GAW   5 items   in 4 days', 'ABC   6 items   in 7 days' ];
-		let third = [ '20%   INR 3000/-   11%', '50%   INR 1000/-   48%', '70%   INR 2000/-   16%' ];
-		const actions = [
-			<FlatButton label="Re-set columns" primary={true} onClick={this.reset} />,
-			<FlatButton label="Done importing" primary={true} onClick={this.done} />
-		];
+		// const actions = [
+		// 	<FlatButton label="Re-set columns" primary={true} onClick={this.reset} />,
+		// 	<FlatButton label="Done importing" primary={true} onClick={this.done} />
+		// ];
 
-		for (let i = 0; i < this.state.data.length; i++) {
-			children.push(
-				<Card>
-					<Card.Content>
-						<Card.Header>
-							Part_No: {' '}
-							<input
-								type="text"
-								value={this.getVal(
-									this.state.data[i]['part no'],
-									this.state.data[i][this.state.propertyName],
-									1
-								)}
-								name="name"
-								style={{ borderWidth: 1, borderColor: this.getBorder(1) }}
-							/>{' '}
-							Qty:
-							<input
-								type="text"
-								value={this.getVal(
-									this.state.data[i]['qty'],
-									this.state.data[i][this.state.propertyName],
-									2
-								)}
-								style={{ borderWidth: 1, borderColor: this.getBorder(2) }}
-								name="name"
-							/>
-							Manufacturer:
-							<input
-								type="text"
-								value={this.getVal(
-									this.state.data[i]['Manufacturer'],
-									this.state.data[i][this.state.propertyName],
-									3
-								)}
-								style={{ borderWidth: 1, borderColor: this.getBorder(3) }}
-								name="name"
-							/>
-							{'   '} Price:
-							<input
-								type="text"
-								value={this.getVal(
-									this.state.data[i]['Price'],
-									this.state.data[i][this.state.propertyName],
-									4
-								)}
-								style={{ borderWidth: 1, borderColor: this.getBorder(4) }}
-								name="name"
-							/>
-							{'   '} Description:
-							<input
-								type="text"
-								value={this.getVal(
-									this.state.data[i]['Description'],
-									this.state.data[i][this.state.propertyName],
-									5
-								)}
-								style={{ borderWidth: 1, borderColor: this.getBorder(5) }}
-								name="name"
-							/>
-						</Card.Header>
-						<Card.Header>{second[i]} </Card.Header>
-						<Card.Description> {third[i]} </Card.Description>
-					</Card.Content>
-				</Card>
-			);
-			/*
+		// for (let i = 0; i < this.state.data.length; i++) {
+		// 	children.push(
+		// 		<Card>
+		// 			<Card.Content>
+		// 				<Card.Header>
+		// 					Part_No: {' '}
+		// 					<input
+		// 						type="text"
+		// 						value={this.getVal(
+		// 							this.state.data[i]['part no'],
+		// 							this.state.data[i][this.state.propertyName],
+		// 							1
+		// 						)}
+		// 						name="name"
+		// 						style={{ borderWidth: 1, borderColor: this.getBorder(1) }}
+		// 					/>{' '}
+		// 					Qty:
+		// 					<input
+		// 						type="text"
+		// 						value={this.getVal(
+		// 							this.state.data[i]['qty'],
+		// 							this.state.data[i][this.state.propertyName],
+		// 							2
+		// 						)}
+		// 						style={{ borderWidth: 1, borderColor: this.getBorder(2) }}
+		// 						name="name"
+		// 					/>
+		// 					Manufacturer:
+		// 					<input
+		// 						type="text"
+		// 						value={this.getVal(
+		// 							this.state.data[i]['Manufacturer'],
+		// 							this.state.data[i][this.state.propertyName],
+		// 							3
+		// 						)}
+		// 						style={{ borderWidth: 1, borderColor: this.getBorder(3) }}
+		// 						name="name"
+		// 					/>
+		// 					{'   '} Price:
+		// 					<input
+		// 						type="text"
+		// 						value={this.getVal(
+		// 							this.state.data[i]['Price'],
+		// 							this.state.data[i][this.state.propertyName],
+		// 							4
+		// 						)}
+		// 						style={{ borderWidth: 1, borderColor: this.getBorder(4) }}
+		// 						name="name"
+		// 					/>
+		// 					{'   '} Description:
+		// 					<input
+		// 						type="text"
+		// 						value={this.getVal(
+		// 							this.state.data[i]['Description'],
+		// 							this.state.data[i][this.state.propertyName],
+		// 							5
+		// 						)}
+		// 						style={{ borderWidth: 1, borderColor: this.getBorder(5) }}
+		// 						name="name"
+		// 					/>
+		// 				</Card.Header>
+		// 				<Card.Header>{second[i]} </Card.Header>
+		// 				<Card.Description> {third[i]} </Card.Description>
+		// 			</Card.Content>
+		// 		</Card>
+		// 	);
+		/*
 			children.push(
 				<tr>
 					<td>{this.state.data[i]['part no']}</td>
@@ -165,24 +162,456 @@ class App extends Component {
 					</tr>
 				);
 			}*/
-		}
+		//}
 
+		var Popup = (
+			<ToolTip
+				downOption={this.state.field}
+				onNext={this.onNext}
+				onBack={this.onBack}
+				onChange={this.onChange}
+				onDone={this.onDone}
+				num={this.state.id}
+			/>
+		);
+		var datas = [];
+		for (let i = 0; i < this.state.data.length; i++) {
+			datas.push(
+				<tr className="lineitem" id="uploaded0">
+					<td className="closer">
+						<div>
+							<a href="#">
+								<i class="fa fa-times" aria-hidden="true" />
+							</a>
+						</div>
+					</td>
+					<td className="line-number-handle">
+						<div>
+							<div className="line-number">{i + 1}</div>
+							<div className="handle">|||</div>
+						</div>
+					</td>
+					<td className="query">
+						<div>
+							<input
+								type="text"
+								style={{ borderWidth: 1, borderColor: this.getBorder(1) }}
+								value="INA_3P96478E"
+							/>
+						</div>
+					</td>
+					<td className="undo-delete" colspan="2">
+						<div>
+							<div>Row deleted</div>
+							<a href="#">Undo</a>
+						</div>
+					</td>
+					<td className="quantity">
+						<div>
+							<input
+								className="form-control"
+								style={{ borderWidth: 1, borderColor: this.getBorder(2) }}
+								type="number"
+								value="1"
+							/>
+						</div>
+					</td>
+					<td className="matched-parts no-matches-found">
+						<div>No matches found</div>
+						<a href="#" className="create-custom-lineitem">
+							Create custom row
+						</a>
+					</td>
+					<td className="lineitem-details">
+						<table>
+							<tbody>
+								<tr>
+									<td className="lineitem-details-column lineitem-details-column-schematicReference">
+										<div>
+											<a href="#" className="edit-link">
+												Edit
+											</a>
+											<div />
+											<a href="#" className="more-link">
+												More...
+											</a>
+										</div>
+									</td>
+									<td className="lineitem-details-column lineitem-details-column-internalPartNumber">
+										<div>
+											<a href="#" className="edit-link">
+												Edit
+											</a>
+											<div />
+											<a href="#" className="more-link">
+												More...
+											</a>
+										</div>
+									</td>
+									<td className="lineitem-details-column lineitem-details-column-description">
+										<div>
+											<a href="#" className="edit-link">
+												Edit
+											</a>
+											<div />
+											<a href="#" className="more-link">
+												More...
+											</a>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+					<td className="selected-distributors">
+						<table>
+							<tbody>
+								<tr>
+									<td className="selected-distributor has-cart">
+										<div>
+											<div className="selected-offer">
+												<div className="no-price">--</div>
+											</div>
+										</div>
+									</td>
+									<td className="selected-distributor has-cart">
+										<div>
+											<div className="selected-offer">
+												<div className="no-price">--</div>
+											</div>
+										</div>
+									</td>
+									<td className="selected-distributor has-cart">
+										<div>
+											<div className="selected-offer">
+												<div className="no-price">--</div>
+											</div>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+					<td className="distributor-sku empty">
+						<div>N/A</div>
+					</td>
+					<td className="unit-price empty">
+						<div>N/A</div>
+					</td>
+					<td className="line-total empty">
+						<div>N/A</div>
+					</td>
+					<td className="batch-total empty">
+						<div>N/A</div>
+					</td>
+					<td className="notes">
+						<div>
+							<a href="#" className="edit-link">
+								Edit
+							</a>
+							<div />
+							<a href="#" className="more-link">
+								More...
+							</a>
+						</div>
+					</td>
+				</tr>
+			);
+		}
 		return (
-			<div className="container">
-				<div style={{ height: 150 }} />
-				{this.state.id < 6 && (
-					<ToolTip
-						num={this.state.id}
-						onChange={this.onChange}
-						onNext={this.onNext}
-						onBack={this.onBack}
-						onDone={this.onDone}
-					/>
-				)}
-				<Dialog actions={actions} modal={true} open={this.state.open}>
-					Everything look okay?
-				</Dialog>
-				{children}
+			<div className="body" style={{ paddingTop: 120 }}>
+				<div className="page bom-tool show">
+					<div id="bombom">
+						<div className="bombom">
+							<h1>
+								<a href="#" className="bom-name">
+									<span>Untitled BOM</span>
+								</a>
+							</h1>
+							<div className="batch-size-pricing">
+								<div className="batch-size">
+									<label>Batch size:</label>
+									<input type="number" value="" />
+								</div>
+								<div className="pricing">
+									<h3 className="bom-total">
+										<small className="currency-code">NOK</small>
+										<span> </span>
+										<span className="amount">0.00</span>
+										<span> each</span>
+									</h3>
+									<h4 className="batch-total-bom-coverage">
+										<div className="batch-total">
+											<small className="currency-code">NOK</small>
+											<span> </span>
+											<span className="amount">0.00</span>
+											<span> total</span>
+										</div>
+										<div className="bom-coverage">
+											<span>0</span>
+											<span>% BOM coverage</span>
+										</div>
+									</h4>
+								</div>
+							</div>
+							<div className="frozen-thead">
+								<table>
+									<thead className="bom-head">
+										<tr className="top">
+											<th className="column-group-selector-heading" colspan="2">
+												<div>
+													<div className="column-group-selector">
+														<a href="#" className="opener">
+															<i className="icon icon_gear" />
+															<i className="icon icon_arrow_down_small" />
+														</a>
+													</div>
+												</div>
+											</th>
+											<th className="item-and-quantity" colspan="2">
+												<div>Part Number &amp; Qty</div>
+											</th>
+											<th className="matched-parts">
+												<div>Matched Parts</div>
+											</th>
+											<th className="lineitem-details-heading-top">
+												<div>
+													<div className="heading">Line Item Details</div>
+													<a className="" href="#">
+														Hide
+													</a>
+												</div>
+											</th>
+											<th className="selected-distributors-heading-top">
+												<div>
+													<div className="heading">Preferred Distributors</div>
+													<a className="" href="#">
+														Hide
+													</a>
+													<a className="" href="#">
+														Edit
+													</a>
+												</div>
+											</th>
+											<th colspan="4" className="cart-pricing-select">
+												<div>
+													<div className="cart">
+														<div>Shopping List</div>
+													</div>
+													<label>Auto fill</label>
+													<div className="pricing-select">
+														<select>
+															<option selected="" value="lowest_overall">
+																Lowest Price (Overall)
+															</option>
+															<option value="lowest_preferred">
+																Lowest Price (Preferred)
+															</option>
+															<option value="2c3be9310496fffc">Digi-Key</option>
+															<option value="a5e060ea85e77627">Mouser</option>
+															<option value="d294179ef2900153">Newark</option>
+															<option value="custom" disabled="">
+																Custom
+															</option>
+														</select>
+														<i className="icon icon_arrow_down_small" />
+													</div>
+													<a className="" href="#">
+														Hide
+													</a>
+												</div>
+											</th>
+											<th className="notes-heading">
+												<div>
+													<a className="heading" href="#">
+														Notes
+													</a>
+
+													<a href="#">Hide</a>
+												</div>
+											</th>
+										</tr>
+										<tr className="bottom">
+											<th className="edit" colspan="2">
+												<div>
+													<a href="#">
+														<span>Edit</span>
+													</a>
+												</div>
+											</th>
+											<th className="item">
+												{this.state.id == 1 ? (
+													<div>
+														<div>Part Number</div>
+														{Popup}
+													</div>
+												) : (
+													<div>Part Number</div>
+												)}
+											</th>
+											<th className="quantity">
+												{this.state.id == 2 ? (
+													<div>
+														<div>Qty</div>
+														{Popup}
+													</div>
+												) : (
+													<div>Qty</div>
+												)}
+											</th>
+											<th className="manufacturer-mpn">
+												{this.state.id == 3 ? (
+													<div>
+														<div>Manufacturer/MPN</div>
+														{Popup}
+													</div>
+												) : (
+													<div>Manufacturer/MPN</div>
+												)}
+											</th>
+											<th className="lineitem-details-heading-bottom">
+												<div>
+													<table>
+														<thead>
+															<tr>
+																<th className="lineitem-details-column schematic-reference">
+																	<div>
+																		{this.state.id == 3 ? (
+																			<div>
+																				<div>Schematic Reference</div>
+																				{Popup}
+																			</div>
+																		) : (
+																			<div>Schematic Reference</div>
+																		)}
+																	</div>
+																</th>
+																<th className="lineitem-details-column internal-part-number">
+																	<div>
+																		{this.state.id == 4 ? (
+																			<div>
+																				<div>Internal Part Number</div>
+																				{Popup}
+																			</div>
+																		) : (
+																			<div>Internal Part Number</div>
+																		)}
+																	</div>
+																</th>
+																<th className="lineitem-details-column description">
+																	<div>
+																		{this.state.id == 5 ? (
+																			<div>
+																				<div>Description</div>
+																				{Popup}
+																			</div>
+																		) : (
+																			<div>Description</div>
+																		)}
+																	</div>
+																</th>
+															</tr>
+														</thead>
+													</table>
+												</div>
+											</th>
+											<th className="selected-distributors-heading-bottom">
+												<div>
+													<table>
+														<thead>
+															<tr>
+																<th className="selected-distributor has-cart">
+																	<div>
+																		<div className="name">Digi-Key</div>
+																		<a href="#">
+																			<small>
+																				<i className="icon icon_cart" />
+																				<span>Buy Now</span>
+																			</small>
+																		</a>
+																	</div>
+																</th>
+																<th className="selected-distributor has-cart">
+																	<div>
+																		<div className="name">Mouser</div>
+																		<a href="#">
+																			<small>
+																				<i className="icon icon_cart" />
+																				<span>Buy Now</span>
+																			</small>
+																		</a>
+																	</div>
+																</th>
+																<th className="selected-distributor has-cart">
+																	<div>
+																		<div className="name">Newark</div>
+																		<a href="#">
+																			<small>
+																				<i className="icon icon_cart" />
+																				<span>Buy Now</span>
+																			</small>
+																		</a>
+																	</div>
+																</th>
+															</tr>
+														</thead>
+													</table>
+												</div>
+											</th>
+											<th className="distributor-sku">
+												<div>
+													<a href="#">Distributor/SKU</a>
+												</div>
+											</th>
+											<th className="unit-price">
+												<div>
+													<a href="#">Unit Price</a>
+												</div>
+											</th>
+											<th className="line-total">
+												<div>Line Total</div>
+											</th>
+											<th className="batch-total">
+												<div>Batch Total</div>
+											</th>
+											<th className="notes-heading-bottom" />
+										</tr>
+									</thead>
+								</table>
+							</div>
+							<table>
+								<tbody className="bom-body ui-sortable">{datas}</tbody>
+								<tfoot className="bom-foot">
+									<tr>
+										<td className="closer" />
+										<td />
+										<td colspan="2" className="add-line-item">
+											<div>
+												<input type="text" placeholder="Add MPN or SKU" value="" />
+												<a href="#" className="pastebox-trigger">
+													Paste
+												</a>
+											</div>
+										</td>
+										<td colspan="8" />
+									</tr>
+									<tr>
+										<td className="closer" />
+										<td />
+										<td className="component-count-label">
+											<div>Component count:</div>
+										</td>
+										<td className="component-count">
+											<div>{this.state.data.length}</div>
+										</td>
+										<td colspan="8" />
+									</tr>
+								</tfoot>
+							</table>
+							<div className="modals" />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -1,54 +1,73 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
+var FontAwesome = require('react-fontawesome');
 const options = [ 'Part Number', 'Qty', 'Schemantic Reference', 'Internal Part Number', 'Description' ];
 const downOption = [ 'part no', 'qty', 'Manufacturer', 'Description', 'Price' ];
 class ToolTip extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { value: props.num, num: props.num - 1, filename: 'sample octo.xslx' };
+		this.state = { downOption: props.downOption, num: props.num - 1, filename: 'sample octo.xslx' };
 	}
 	componentWillReceiveProps(props) {
-		this.setState({ value: props.num, num: props.num - 1 });
+		this.setState({ downOption: props.downOption, num: props.num - 1 });
 	}
-	handleChange = (event, index, value) => {
-		this.props.onChange(downOption[value - 1]);
-		this.setState({ value });
-	};
 
 	render() {
 		return (
-			<div style={{ backgroundColor: 'grey', width: 600, padding: 30 }}>
-				<span>
-					Choose the column from your file to use for the <span>{options[this.state.num]}</span> column
-				</span>
-				<DropDownMenu value={this.state.value} onChange={this.handleChange}>
-					<MenuItem value={1} primaryText="part no" />
-					<MenuItem value={2} primaryText="qty" />
-					<MenuItem value={3} primaryText="Manufacturer" />
-					<MenuItem value={4} primaryText="Description" />
-					<MenuItem value={5} primaryText="Price" />
-				</DropDownMenu>
-				<p style={{ height: 20 }} />
-				<span>Step {this.state.num + 1} of 5</span>
-				{this.state.num != 0 && (
-					<button style={{ marginLeft: 200, marginRight: 20 }} onClick={this.props.onBack}>
-						{' '}
-						Back
-					</button>
-				)}
-				<button
-					style={{ marginLeft: this.state.num == 0 ? 200 : 0, marginRight: 20 }}
-					onClick={this.props.onNext}
-				>
-					{' '}
-					Next{' '}
-				</button>
-				<button style={{ marginRight: 20 }} onClick={this.props.onDone}>
-					{' '}
-					Done{' '}
-				</button>
+			<div class="choose-column-tooltip">
+				<div>
+					<div class="prompt">
+						<div class="instructions">
+							<span>Choose the column from your file to use for the </span>
+							<span class="column-name">{options[this.state.num]}</span>
+							<span> column.</span>
+						</div>
+						<div class="filename">{this.state.filename}</div>
+					</div>
+					<form>
+						<div class="column-select-container">
+							<div class="column-select">
+								<select>
+									{this.state.num != 0 && <option>[skip this column]</option>}
+									{this.state.downOption.map((val, id) => (
+										<option
+											value={id}
+											onClick={() => this.props.onChange(this.state.downOption[id])}
+										>
+											{this.state.downOption[id]}
+										</option>
+									))}
+								</select>
+								<FontAwesome
+									className="super-crazy-colors"
+									name="angle-down"
+									size="1x"
+									style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+								/>
+							</div>
+						</div>
+						<div class="step-controls">
+							<div class="step">
+								<span>Step </span>
+								<span>{this.state.num + 1}</span>
+								<span> of </span>
+								<span>5</span>
+							</div>
+							<div class="controls">
+								{this.state.num != 0 && (
+									<a class="back" href="#" onClick={this.props.onBack}>
+										<span>Back</span>
+									</a>
+								)}
+								<a class="next" href="#" onClick={this.props.onNext}>
+									<span>Next</span>
+								</a>
+								<a class="done" href="#" onClick={this.props.onDone}>
+									Done
+								</a>
+							</div>
+						</div>
+					</form>
+				</div>
 			</div>
 		);
 	}
